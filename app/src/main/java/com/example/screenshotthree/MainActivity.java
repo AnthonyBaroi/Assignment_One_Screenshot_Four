8739399e3d68f961324d8df92cd05ff911c2e3f7
AnthonyBaroi <69390725+AnthonyBaroi@users.noreply.github.com>
2021-11-17 20:04:28 +0600

    Initial commit

---
+++ app/src/main/java/com/example/screenshotthree/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.screenshotthree;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
